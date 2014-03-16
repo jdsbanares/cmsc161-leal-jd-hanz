ebab22cd3d5e458d3be989afe03e3b206c6ca516
anbaIAN <adrian.leal23@gmail.com>
2014-03-16 10:14:32 +0800

    added full assigned task

--- ceiling.js
+++ ceiling.js
@@ -1,193 +1,179 @@
+function create_ceiling_tile(gl, program, canvas, x, y, z, rotateX, rotateY, rotateZ){
+
+	// Coordinates
+	var vertices = [
+		 0.00,  0.00,  0.00,
+		 0.10,  0.00,  0.00,
+		 0.10,  0.00,  0.10,
+		 0.00,  0.00,  0.10,
+		 0.00,  0.03,  0.00,
+		 0.10,  0.03,  0.00,
+		 0.10,  0.03,  0.10,
+		 0.00,  0.03,  0.10
+	];
+
+	// Normal of each vertex
+	var normals = [
+		 0.0,  0.0, -1.0,
+		 0.0,  0.0, -1.0,
+		 0.0,  0.0, -1.0,
+		 0.0,  0.0, -1.0,
+		 0.0,  0.0,  1.0,
+		 0.0,  0.0,  1.0,
+		 0.0,  0.0,  1.0,
+		 0.0,  0.0,  1.0
+	];
+
+	// Indices of the vertices
+	var indices = [
+		 0,  1,  2,
+		 0,  2,  3,
+		 4,  5,  6,
+		 4,  6,  7,
+		 2,  3,  7,
+		 2,  7,  6,
+		 3,  0,  4,
+		 3,  4,  7,
+		 0,  1,  5,
+		 0,  5,  4,
+		 1,  2,  6,
+		 1,  6,  5
+	];
+	
+	// Coordinates
+	var tex_coords = [
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0,	//front
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0,	//right
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0,	//up
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0,	//left
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0,	//down
+		1.0, 1.0,	0.0, 1.0,	0.0, 0.0,	1.0, 0.0	//back
+	];
+	
+	var verticesBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	//attribute variable mapping to buffer
+	var aPosition = gl.getAttribLocation(program,"aPosition");
+	gl.vertexAttribPointer(aPosition,3,gl.FLOAT,false,0,0);
+	gl.enableVertexAttribArray(aPosition);
+	//unbind buffer to ARRAY_BUFFER POINTER
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	var normalBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+	//attribute variable mapping to buffer
+	var aNormal = gl.getAttribLocation(program,"aNormal");
+	gl.vertexAttribPointer(aNormal,3,gl.FLOAT,false,0,0);
+	gl.enableVertexAttribArray(aNormal);
+	//unbind buffer to ARRAY_BUFFER POINTER
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	//buffer creation
+	var indexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+	//unbind buffer to gl.ELEMENT_ARRAY_BUFFER POINTER
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null); 
+
+	
+	//buffer creation
+	var texCoordsBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tex_coords), gl.STATIC_DRAW);
+	//attribute variable mapping to buffer
+	var aTexCoords = gl.getAttribLocation(program,"aTexCoords");
+	gl.vertexAttribPointer(aTexCoords,2,gl.FLOAT,false,0,0);
+	gl.enableVertexAttribArray(aTexCoords);
+	//unbind buffer to ARRAY_BUFFER POINTER
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 	
 
-		var image;
-		function ceiling(x,y,z,rotatex,rotatey,rotatez) {
-			var canvas = document.getElementById("c");
-			var gl = initializeWebGL(canvas);
-			var l=0.2, w=0.1, h=0.05;
-			
-			var vertexShader = initializeShader(gl,"vshader");
-			var fragmentShader = initializeShader(gl, "fshader");
-			var program = initializeProgram(gl,vertexShader,fragmentShader);
-			gl.useProgram(program);
-			
+	//set-up model matrix, view matrix, and projection matrix
+	var modelMatrix = mat4.create();
+	var uModel = gl.getUniformLocation(program,"uModel");
+	mat4.translate(modelMatrix,modelMatrix,[x,y,z]);
+	mat4.rotateX(modelMatrix,modelMatrix,rotateX);
+	mat4.rotateY(modelMatrix,modelMatrix,rotateY);
+	mat4.rotateZ(modelMatrix,modelMatrix,rotateZ);
+	gl.uniformMatrix4fv(uModel,false,modelMatrix);
 
-		  	var cube_vertices = [   // Coordinates
-		     x+w,y+h,z+w,	x-w,y+h,z+w,	x-w,y-h,z+w,	x+w,y-h,z+w, //front
-		     x+w,y+h,z+w,	x+w,y-h,z+w,	x+w,y-h,z-w,	x+w,y+h,z-w, //right
-		     x+w,y+h,z+w,	x+w,y+h,z-w,	x-w,y+h,z-w,	x-w,y+h,z+w, //up
-		     x-w,y+h,z+w,	x-w,y+h,z-w,	x-w,y-h,z-w,	x-w,y-h,z+w,//left
-		     x-w,y-h,z-w,	x+w,y-h,z-w,	x+w,y-h,z+w,	x-w,y-h,z+w,//down
-		     x+w,y-h,z-w,	x-w,y-h,z-w,	x-w,y+h,z-w,	x+w,y+h,z-w//back
-		  	];
-		  	//buffer creation
-		  	var cubeVerticesBuffer = gl.createBuffer();
-		  	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
-		  	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube_vertices), gl.STATIC_DRAW);
-		  	//attribute variable mapping to buffer
-		  	var aPosition = gl.getAttribLocation(program,"aPosition");
-		  	gl.vertexAttribPointer(aPosition,3,gl.FLOAT,false,0,0);
-		  	gl.enableVertexAttribArray(aPosition);
-		  	//unbind buffer to ARRAY_BUFFER POINTER
-		  	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-		  	
-		  	// Indices of the vertices
-			var indices = [
-			 0, 1, 2,   0, 2, 3,    // front
-			 4, 5, 6,   4, 6, 7,    // right
-			 8, 9,10,   8,10,11,    // up
-			12,13,14,  12,14,15,    // left
-			16,17,18,  16,18,19,    // down
-			20,21,22,  20,22,23     // back
-			];
-			//buffer creation
-		  	var indexBuffer = gl.createBuffer();
-		  	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-		  	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-		  	//unbind buffer to gl.ELEMENT_ARRAY_BUFFER POINTER
-		  	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null); 
+	//add normal matrix
+	var normalMatrix = mat4.create();
+	var uNormal = gl.getUniformLocation(program,"uNormal");
+	mat4.invert(normalMatrix,modelMatrix);
+	mat4.transpose(normalMatrix,normalMatrix);
+	gl.uniformMatrix4fv(uNormal,false,normalMatrix);
 
-		  	var tex_coords = [   // Coordinates
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0,
+	var viewMatrix = mat4.create();
+	var uView = gl.getUniformLocation(program,"uView");
+	//mat4.lookAt(viewMatrix,[2,0.25,2],[0,0.25,0],[0,1,0]);
+	mat4.lookAt(viewMatrix,[1,5.25,0],[0,0.25,0],[0,1,0]);
+	gl.uniformMatrix4fv(uView,false,viewMatrix);
 
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0,
+	var projectionMatrix = mat4.create();
+	var uProjection = gl.getUniformLocation(program,"uProjection");
+	mat4.perspective(projectionMatrix,glMatrix.toRadian(30),canvas.width/canvas.height,1,100);
+	//mat4.ortho(projectionMatrix,2,2,2,2,1,5);
+	gl.uniformMatrix4fv(uProjection,false,projectionMatrix);
 
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0,
+	//set-up light and material parameters
 
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0,
+	var uLightDirection= gl.getUniformLocation(program,"uLightDirection");
+	gl.uniform3f(uLightDirection,-1.0,-1.0,-1.0);
 
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0,
+	var uEyePosition= gl.getUniformLocation(program,"uEyePosition");
+	gl.uniform3f(uEyePosition,1,1,1);
 
-		     1.0, 1.0,  
-		     0.0, 1.0, 
-		     0.0, 0.0, 
-		     1.0, 0.0
-		  	];
-		  	//buffer creation
-		  	var texCoordsBuffer = gl.createBuffer();
-		  	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsBuffer);
-		  	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tex_coords), gl.STATIC_DRAW);
-		  	//attribute variable mapping to buffer
-		  	var aTexCoords = gl.getAttribLocation(program,"aTexCoords");
-		  	gl.vertexAttribPointer(aTexCoords,2,gl.FLOAT,false,0,0);
-		  	gl.enableVertexAttribArray(aTexCoords);
-		  	//unbind buffer to ARRAY_BUFFER POINTER
-		  	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-		  	
-		  	
+	var uMaterialDiffuse = gl.getUniformLocation(program,"uMaterialDiffuse");
+	gl.uniform3f(uMaterialDiffuse,0.2,0.7,0.2);
 
-		  	//set-up model matrix, view matrix, and projection matrix
-		  	var modelMatrix = mat4.create();
-		  	var uModel = gl.getUniformLocation(program,"uModel");
-		  	//mat4.rotateX(modelMatrix,modelMatrix,glMatrix.toRadian(0));//??change rotation angle
-		  	gl.uniformMatrix4fv(uModel,false,modelMatrix);
+	var uLightDiffuse = gl.getUniformLocation(program,"uLightDiffuse");
+	gl.uniform3f(uLightDiffuse,1.0,1.0,1.0);
 
-		  	var viewMatrix = mat4.create();
-		  	var uView = gl.getUniformLocation(program,"uView");
-		  	mat4.lookAt(viewMatrix,[-0.5,-0.5,2],[0,0,0],[0,1,0]);
-		  	gl.uniformMatrix4fv(uView,false,viewMatrix);
+	var uMaterialAmbient = gl.getUniformLocation(program,"uMaterialAmbient");
+	gl.uniform3f(uMaterialAmbient,0.0,0.5,0.0);
 
-		  	var projectionMatrix = mat4.create();
-		  	var uProjection = gl.getUniformLocation(program,"uProjection");
-		  	mat4.perspective(projectionMatrix,glMatrix.toRadian(30),canvas.width/canvas.height,1,100);
-		  	gl.uniformMatrix4fv(uProjection,false,projectionMatrix);
-		  	
+	var uLightAmbient = gl.getUniformLocation(program,"uLightAmbient");
+	gl.uniform3f(uLightAmbient,0.3,0.3,0.3);
 
-		  	var texture = gl.createTexture();
-		  	var uSampler = gl.getUniformLocation(program, 'uSampler');
-  			var image = new Image(); 
-  			var image1Ready = false;
-			image.onload = function(){ 
-				gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
-				gl.activeTexture(gl.TEXTURE0);
-				gl.bindTexture(gl.TEXTURE_2D, texture);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-				gl.generateMipmap(gl.TEXTURE_2D);
+	var uMaterialSpecular = gl.getUniformLocation(program,"uMaterialSpecular");
+	gl.uniform3f(uMaterialSpecular,0.9,1.0,0.9);
 
-				gl.uniform1i(uSampler, 0);
-				image1Ready = true;
-			};
-  			image.src = 'textures/4.jpg';
+	var uLightSpecular = gl.getUniformLocation(program,"uLightSpecular");
+	gl.uniform3f(uLightSpecular,1.0,1.0,1.0);
 
-  			/*var isDragged = false;
-  			var prevX = -1;
-  			var prevY = -1;
+	var uShininess = gl.getUniformLocation(program,"uShininess");
+	gl.uniform1f(uShininess,1000.0);
 
-  			var xRotation = 0;
-  			var yRotation = 0;
-  			canvas.onmousedown = function(ev) {
-  				var canvasBounds = ev.target.getBoundingClientRect();
-  				var currX = ev.clientX;
-  				var currY = ev.clientY;
-  				if (canvasBounds.left <= currX && currX < canvasBounds.right && canvasBounds.top <= currY && currX < canvasBounds.bottom) {
-      				prevX = currX; 
-      				prevY = currY;
-      				isDragged = true;
-    			}
-  			};
-  			canvas.onmouseup = function(ev) {
-  				isDragged = false;
-  			};
-  			window.onmouseup = function(ev) {
-  				isDragged = false;
-  			};
-  			canvas.onmousemove = function(ev) {
-  				var currX = ev.clientX;
-  				var currY = ev.clientY;
+	var uEnableAmbient = gl.getUniformLocation(program,"uEnableAmbient");
+	gl.uniform1i(uEnableAmbient,true);
 
-  				if(isDragged) {
-  					var factor = 100/canvas.height; // The rotation ratio
-      				var dx = factor * (currX - prevX);
-      				var dy = factor * (currY - prevY);
-      				xRotation += dy;
-  					xRotation %= 360;
-  					yRotation += dx;
-  					yRotation %= 360;
-  					//console.log(xRotation + " " + yRotation);
-  			
-  				}
-  				prevX = currX; 
-      			prevY = currY;
-  			};*/
+	var uEnableDiffuse = gl.getUniformLocation(program,"uEnableDiffuse");
+	gl.uniform1i(uEnableDiffuse,true);
 
-  			var animate = function() {
-  				gl.clearColor(0, 0, 0, 1);
-  				gl.enable(gl.DEPTH_TEST);
-				gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	var uEnableSpecular = gl.getUniformLocation(program,"uEnableSpecular");
+	gl.uniform1i(uEnableSpecular,true);
 
-				/*modelMatrix = mat4.identity(modelMatrix);
-  				mat4.rotateX(modelMatrix,modelMatrix,glMatrix.toRadian(xRotation));
-  				mat4.rotateY(modelMatrix,modelMatrix,glMatrix.toRadian(yRotation));
-  				gl.uniformMatrix4fv(uModel,false,modelMatrix);*/
-  				
-  				if(image1Ready) {	
-					mat4.scale(modelMatrix,modelMatrix,[1,1,1]);
-					gl.uniformMatrix4fv(uModel,false,modelMatrix);		
-					gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-					gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-					
-				}
-				
-				requestAnimFrame(animate);
-			}
-			
-			//start animation
-			animate();
-		}
+	var texture = gl.createTexture();
+	var uSampler = gl.getUniformLocation(program, 'uSampler');
+	var image = new Image();
+	image.onload = function(){ 
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+		gl.uniform1i(uSampler, 0);
+		
+		//draw scene when the image has loaded  
+		gl.clearColor(0, 0, 0, 1);
+		gl.enable(gl.DEPTH_TEST);
+		gl.uniformMatrix4fv(uModel,false,modelMatrix);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+		gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+	};
+	image.src = 'textures/4.jpg';
+	
+}
